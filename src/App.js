76f3a0402ae029e8f1885164fc6b1a7cc116d8ec
renatobenks <renato-benkendorf@hotmail.com>
2018-02-14 23:49:45 -0200

    create button at react home

--- src/App.js
+++ src/App.js
@@ -3,6 +3,20 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isClicked: true,
+    };
+  }
+
+  onClickButton() {
+    alert('Youve clicked at button, no more clicks for today');
+    this.setState({
+      isClicked: false,
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +24,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <button disabled={!this.state.isClicked} onClick={() => this.onClickButton()}>Click me!</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
